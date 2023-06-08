c6d6a96a72a628bfaa2d27d09c9152032f2d04a9
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-08 05:48:09 -0500

    elaborado linea de piezas

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,2 +1,15 @@
 from interpreter import draw
 from chessPictures import *
+
+
+sqRock = square.negative().under(rock) 
+sqKnight = square.under(knight)
+sqBishop = square.negative().under(bishop) 
+sqQueen = square.under(queen)
+sqKing = square.negative().under(king) 
+sqBishop2 = square.under(bishop)
+sqKnight2 = square.negative().under(knight) 
+sqRock2 = square.under(rock)
+
+linePieces = sqRock.join(sqKnight).join(sqBishop).join(sqQueen).join(sqKing).join(sqBishop2).join(sqKnight2).join(sqRock2)
+draw(linePieces)
