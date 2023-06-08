241bf13255dc68a9795aa21eb19843f19f74d0d6
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-06-08 09:50:54 -0500

    añadido comentarios

--- picture.py
+++ picture.py
@@ -13,42 +13,42 @@ class Picture:
 
   def verticalMirror(self):
     """ Devuelve el espejo vertical de la imagen """
-    vertical = []
-    for value in self.img:
-    	vertical.append(value[::-1])
-    return Picture(vertical)
+    vertical = []                     # Creacion de vertical
+    for value in self.img:            
+    	vertical.append(value[::-1])    # Cada linea de la imagen es volteada 
+    return Picture(vertical)          # Se retorna objeto Picture
 
   def horizontalMirror(self):
     """ Devuelve el espejo horizontal de la imagen """
-    horizontal = self.img[::-1]
-
+    horizontal = self.img[::-1]       # En vez de la linea se voltea los elementos
+                                      # de la lista de lineas de la imagen
     return Picture(horizontal)
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
-    negative = []
+    negative = []                     # Se crea negative
 
     for line in self.img:
-      newLine = ""
-      for i in line:
+      newLine = ""                    # Se reemplaza cada punto de la imagen 
+      for i in line:                  # Con su negativo en color
         newLine += self._invColor(i)
-      negative.append(newLine)
+      negative.append(newLine)        # Se agrega la linea creada al negative
     return Picture(negative)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    joinImg = []
+    joinImg = []                      #
 
-    for line in range(len(self.img)):
-      joinImg.append(self.img[line]+p.img[line])
+    for line in range(len(self.img)): # Concatenamos cada linea de la imagen con cada
+      joinImg.append(self.img[line]+p.img[line]) # linea de P
 
     return Picture(joinImg)
 
   def up(self, p):
     """Devuelve una nueva figura poniendo la figura p debajo de
        la figura actual """
-    upImg = self.img + p.img
+    upImg = self.img + p.img        # juntamos ambas listas de lineas
     
     return Picture(upImg)
 
@@ -60,12 +60,12 @@ class Picture:
 
     underImg = []
     
-    for line in range(length):
+    for line in range(length):        # 
       item = ""
       for i in range(length):
-        if p.img[line][i] == " ":
-          item += self.img[line][i]
-        else:
+        if p.img[line][i] == " ":     # Cada punto blanco en la imagen p 
+          item += self.img[line][i]   # Es reemplazada por la pocicion en
+        else:                         # la imagen self.img
           item += p.img[line][i]
       underImg.append(item)      
 
@@ -76,11 +76,11 @@ class Picture:
         la cantidad de veces que indique el valor de n """
     repeat = []
     for line in self.img:
-      repeat.append(line*n)
+      repeat.append(line*n)           # Multiplicamos cada linea de self.img por n
     return Picture(repeat)
 
   def verticalRepeat(self, n):
-    repeat = self.img * n
+    repeat = self.img * n             # Multiplicamos la imagen por n veces
     return Picture(repeat)
 
   #Extra: Sólo para realmente viciosos 
@@ -88,14 +88,14 @@ class Picture:
     """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
     o antihorario"""
 
-    length = len(self.img)
+    length = len(self.img)            # Extremos el numero de linas
     rotateImg = []
 
     for i in range(length):
-      rotateLine = ""  
+      rotateLine = ""                 # creamos una linea
       for line in self.img:
-        rotateLine += line[i]
-      rotateImg.append(rotateLine)
+        rotateLine += line[i]         # cada linea va a ser un string de caracteres de la pocicion 
+      rotateImg.append(rotateLine)    # i de cada linea de la imagen
 
     return Picture(rotateImg)
 
